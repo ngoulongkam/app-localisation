d7579d9aac53ecd4b08a934e1e173a9f1917ef42
ngoulongkam <25244131+ngoulongkam@users.noreply.github.com>
2022-08-22 14:57:53 +0100

    first commit

---
+++ app/src/main/java/com/example/app_localisation/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.app_localisation
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.os.LocaleListCompat
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+class MainActivity : AppCompatActivity() {
+    private var languageMenuItem: MenuItem? = null
+    private var shouldShowLangToggle: Boolean = true
+    private var isInEng: Boolean = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState != null) {
+            isInEng = savedInstanceState.getBoolean("isInEng")
+        }
+        setContentView(R.layout.activity_main)
+
+        title = getString(R.string.screen_title)
+        val dialogButton = findViewById<Button>(R.id.dialog_button)
+
+        dialogButton.setOnClickListener {
+            AlertDialog.Builder(this).create().apply {
+                setTitle(getString(R.string.dialog_title))
+                setMessage(getString(R.string.dialog_text))
+                show()
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean("isInEng", isInEng)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if (shouldShowLangToggle) {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.icon, menu)
+        if (menu != null) {
+            languageMenuItem = menu.findItem(R.id.action_language)
+                val setIcon = if (isInEng) R.drawable.ic_welsh else R.drawable.ic_english
+                languageMenuItem?.icon = ResourcesCompat.getDrawable(resources, setIcon, null)
+            }
+        } else {
+            updateAppLocales("en_rGB")
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_language -> {
+                val lang = if (isInEng) "cy" else "en_rGB"
+                updateAppLocales(lang)
+                isInEng = !isInEng
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    private fun updateAppLocales(locales: String) {
+        val appLocale: LocaleListCompat = LocaleListCompat.forLanguageTags(locales)
+        AppCompatDelegate.setApplicationLocales(appLocale)
+    }
+}
